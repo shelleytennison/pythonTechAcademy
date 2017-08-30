8b20b67ce951ddd05f89a04718b8899d2f7d76a3
shelleytennison <tennison.shelley@gmail.com>
2017-08-30 13:17:23 -0700

    Add files via upload

--- PythonItem46.py
+++ PythonItem46.py
@@ -2,14 +2,15 @@
 # Python 3.6.2
 
 
-my_list = [0,1,2,3]
+
 for i in range(4):
-    print(my_list[i])
+    print(i)
 
 
-print (my_list[::-1])
+for a in range(4, -1, -1):
+    print(a)
+    
 
 
-my_list2 = [0,1,2,3,4,5,6,7,8]
-for i in range(8,1,-2):
-    print (my_list2[i])
+for b in range(8,1,-2):
+    print (b)
