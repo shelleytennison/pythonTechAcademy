1a7c86ae6a61d54b89d3807580a9ad8361c5a3a7
shelleytennison <tennison.shelley@gmail.com>
2017-08-30 13:19:21 -0700

    Add files via upload

--- PythonItem48.py
+++ PythonItem48.py
@@ -10,18 +10,10 @@ def bubbleSort(alist):
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
 alist = [67, 45, 2, 13, 1, 998]
+blist = [89,23,33,45,10,12,45,45,45]
 bubbleSort(alist)
 print (alist)
-
-
-
-def bubbleSort(blist):
-    for passnum in range(len(blist)-1,0,-1):
-        for i in range(passnum):
-            if blist[i]>blist[i+1]:
-                temp = blist[i]
-                blist[i] = blist[i+1]
-                blist[i+1] = temp
-blist = [89,23,33,45,10,12,45,45,45]
 bubbleSort(blist)
 print (blist)
+
+
