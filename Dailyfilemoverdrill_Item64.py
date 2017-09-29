ff0f623c5b362af420951c5d6a402c882789e38a
shelleytennison <tennison.shelley@gmail.com>
2017-09-29 13:42:41 -0700

    Add files via upload

--- Dailyfilemoverdrill_Item64.py
+++ Dailyfilemoverdrill_Item64.py
@@ -8,9 +8,13 @@ dir_src = ("C:\\Users\shell\Desktop\\New Customer Orders\\")
 dir_dst = ("C:\\Users\shell\Desktop\\All Customer Orders\\")
 
 currentTime = time.time()
-modifiedTime = os.path.getmtime(dir_src)
+print(currentTime)
 
 for filename in os.listdir(dir_src):
+    modifiedTime = os.path.getmtime(dir_src + filename)
+    
     if filename.endswith('.txt') and currentTime - modifiedTime < 86400:
         shutil.move( dir_src + filename, dir_dst)
+    
     print(filename)
+    print(currentTime-modifiedTime)
